0403376c9bf88eb2ff09bac8b208a957f521c0dc
Hamsa Mohammed <hamsam1197@gmail.com>
2021-10-14 16:14:42 +0100

    Add item + basket class, add tests up to remove

--- spec/bobs-bagels.spec.js
+++ spec/bobs-bagels.spec.js
@@ -1,26 +1,71 @@
-const assertEquals = require('../test-frameworks');
-const Bagel = require('../src/bobs-bagels');
+const test = require('../test-frameworks');
+const Item = require('../src/Item');
+const Basket = require('../src/Basket');
 
-let input;
-let output;
-let result;
+test.it('Checking if Bagel has name', () => {
+    let input;
+    let expectedOutput;
+    let result;
+    let cheese = new Item('cheese');
 
-// Test 1 //
-
-let cheese = new Bagel('cheese');
-
-
-input = 'cheese';
-output = 'cheese';
-result = cheese.name;
-
-console.log(assertEquals(output, result));
-
-//Test 2//
+    input = 'cheese';
+    expectedOutput = 'cheese';
+    result = cheese.name;
+    test.assertEquals(expectedOutput, result);
+})
 
 
-let plain = new Bagel('plain');
-output = 'plain';
-result = plain.name2();
+test.it('Testing if basket has item', () => {
+    let input;
+    let expectedOutput;
+    let result;
 
-console.log(assertEquals(output, result));
+    let basket = new Basket();
+    let bagel1 = new Item('bagel');
+    basket.addItem(bagel1);
+
+    expectedOutput = 1;
+    input = bagel1;
+    result = basket.items.length
+    test.assertEquals(expectedOutput, result);
+});
+
+
+
+test.it('Testing if we can remove item from basket', () => {
+    let input;
+    let expectedOutput;
+    let result;
+
+    let basket = new Basket();
+    let bagel1 = new Item('bagel');
+    basket.addItem(bagel1);
+    basket.removeItem();
+
+    expectedOutput = 0;
+    input = bagel1;
+    result = basket.items.length
+    test.assertEquals(expectedOutput, result);
+});
+
+
+test.it('Testing if we can remove specific item from basket', () => {
+    let input;
+    let expectedOutput;
+    let result;
+
+    let basket = new Basket();
+    let bagel1 = new Item('bagel');
+    let bagel2 = new Item('bagel');
+    basket.addItem(bagel1);
+    basket.addItem(bagel2);
+    basket.removeItem(bagel1);
+
+    expectedOutput = true;
+    input = bagel2;
+    result = basket.items.includes(input);
+    test.assertEquals(expectedOutput, result);
+});
+
+
+
